6b7b155b0b8f6dbcb7419e1fb8460527d0e89433
Yongwei Xing <yongwei.xing@liulishuo.com>
2018-12-21 13:30:40 +0800

    remove files

--- layers.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def fc(z, W, b):
-    return np.dot(z, W) + b
-
-
-def fc_bp(ndz, W, z):
-    n = z.shape[0]
--- loss.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def mse(pred, y):
-    loss = np.mean(np.sum(np.square(pred - y), axis=-1))
-    grad = pred - y
-    return loss, grad
